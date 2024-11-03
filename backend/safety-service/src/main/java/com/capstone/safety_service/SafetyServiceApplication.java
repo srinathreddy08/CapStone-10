d71d201a9bae5d5a83db9b6322570c7ebc4996e1
srinathreddy08 <reddysrinath2002@gmail.com>
2024-11-03 15:39:48 +0530

    safety service

---
+++ backend/safety-service/src/main/java/com/capstone/safety_service/SafetyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.safety_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyServiceApplication.class, args);
+	}
+
+}
